914c47dddfa07cf48df5eb492eadd4e4a2a36d1d
Peter Rigii <theonlyrigii@gmail.com>
2023-06-06 18:14:53 +0300

    isupper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,5 +5,5 @@ def uppercase(string):
             uppercase_char = chr(ord(char) - 32)
         else:
             uppercase_char = char
-        print(uppercase_char, end='')
+        print({}.format(uppercase_char), end='')
     print()
