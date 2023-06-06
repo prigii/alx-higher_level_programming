bd6197817e2df0ca9bb107eefd1b257231447742
Peter Rigii <theonlyrigii@gmail.com>
2023-06-06 16:41:23 +0300

    alphabet games 2

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z') + 1):
     if chr(i) not in ['q', 'e']:
-        print(chr(i), end='')
-print()
+        print(f"{chr(i)}".format(range), end='')
