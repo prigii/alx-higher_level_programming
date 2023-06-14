9e8539d36f15440c0ad9a7727c8d91618444abc2
Peter Rigii <theonlyrigii@gmail.com>
2023-06-14 20:59:41 +0300

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -20,12 +20,5 @@ def search_replace(my_list, search, replace):
     a new list with the new elements
     """
 
-    new_list = my_list[:]
-
-    for i in range(len(new_list)):
-
-        if new_list[i] == search:
-
-            new_list[i] = replace
-
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
         return (new_list)
