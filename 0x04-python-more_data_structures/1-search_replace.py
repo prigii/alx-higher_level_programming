d33198f305b3ef8ba5e28ebef9f43292b73c5236
Peter Rigii <theonlyrigii@gmail.com>
2023-06-15 08:27:25 +0300

    search and replace debug

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -20,6 +20,5 @@ def search_replace(my_list, search, replace):
     a new list with the new elements
     """
 
-
-    new_list = list(map(lambda x: replace if x == search else x for x in my_list))
-        return (new_list)
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
+    return (new_list)
