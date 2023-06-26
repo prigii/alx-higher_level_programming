cb88f38ca7bdf6c0152d2a9c7a392b4204672840
Peter Rigii <theonlyrigii@gmail.com>
2023-06-26 20:59:44 +0300

    safe print integer

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,9 +4,8 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        integer_value = int(value)
-        print("{:d}".format(integer_value))
-    except ValueError:
-        print("Exception: Invalid integer value", file=sys.stderr)
+        print("{:d}".format(value))
+    except Exception as ex:
+        print("Exception: {}".format(ex), file=sys.stderr)
         return False
     return True
