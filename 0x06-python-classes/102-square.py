0bf16aec7522364a008168cdf7e6e5e9a6e1c061
Peter Rigii <theonlyrigii@gmail.com>
2023-06-28 00:32:37 +0300

    102 square

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -6,72 +6,118 @@ Define a class Square
 
 
 class Square:
+
     """
-        Represent a square
-	    """
 
-	        def __init__(self, size=0):
-		        """
-			        Initialize a new square
+    Represent a square
 
-				        Args:
-					            size (int): The size of the new square
-						            """
-							            self.size = size
+    """
 
-								        @property
-									    def size(self):
-									            """
-										            Get/set the current size of the square
-											            """
-												            return self.__size
+    def __init__(self, size=0):
 
-													        @size.setter
-														    def size(self, value):
-														            if not isinstance(value, int):
-															                raise TypeError("size must be an integer")
-																	        elif value < 0:
-																		            raise ValueError("size must be >= 0")
-																			            self.__size = value
+        """
+        Initialize a new square
 
-																				        def area(self):
-																					        """
-																						        Return the current area of the square
-																							        """
-																								        return self.__size * self.__size
+        Args:
 
-																									    def __eq__(self, other):
-																									            """
-																										            Define the equal comparison to a Square
-																											            """
-																												            return self.area() == other.area()
+        size (int): The size of the new square
 
-																													        def __ne__(self, other):
-																														        """
-																															        Define the not equal comparison to a Square
-																																        """
-																																	        return self.area() != other.area()
+        """
 
-																																		    def __lt__(self, other):
-																																		            """
-																																			            Define the less than comparison to a Square
-																																				            """
-																																					            return self.area() < other.area()
+        self.size = size
 
-																																						        def __le__(self, other):
-																																							        """
-																																								        Define the less than or equal to comparison to a Square
-																																									        """
-																																										        return self.area() <= other.area()
+    @property
+    def size(self):
 
-																																											    def __gt__(self, other):
-																																											            """
-																																												            Define the greater than comparison to a Square
-																																													            """
-																																														            return self.area() > other.area()
+        """
 
-																																															        def __ge__(self, other):
-																																																        """
-																																																	        Define the greater than or equal to comparison to a Square
-																																																		        """
-																																																			        return self.area() >= other.area()
+        Get/set the current size of the square
+
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+
+        if not isinstance(value, int):
+
+            raise TypeError("size must be an integer")
+
+        elif value < 0:
+
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+
+        """
+        Return the current area of the square
+        
+        """
+
+        return self.__size * self.__size
+	
+    def __eq__(self, other):
+
+        """
+
+        Define the equal comparison to a Square
+
+        """
+
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+
+        """
+
+        Define the not equal comparison to a Square
+
+        """
+
+        return self.area() != other.area()
+
+		
+    def __lt__(self, other):
+
+        """
+
+        Define the less than comparison to a Square
+
+        """
+
+        return self.area() < other.area()
+
+		
+    def __le__(self, other):
+
+        """
+
+        Define the less than or equal to comparison to a Square
+
+        """
+
+        return self.area() <= other.area()
+
+		
+    def __gt__(self, other):
+
+        """
+
+        Define the greater than comparison to a Square
+
+        """
+
+            return self.area() > other.area()
+
+			
+    def __ge__(self, other):
+
+        """
+
+        Define the greater than or equal to comparison to a Square
+
+        """
+
+        return self.area() >= other.area()
