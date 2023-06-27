5900e7c34c367b98a5a22a7a3075073a43bb4be5
Peter Rigii <theonlyrigii@gmail.com>
2023-06-27 17:13:59 +0300

    oop

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Defines a class square
+"""
+
+
+class Square:
+    """
+        Represent a square
+	    """
+	        pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,2 @@
+0x06. Python - Classes and Objects Project Tasks
+Object Oriented Programming
