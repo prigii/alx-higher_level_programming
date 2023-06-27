1cb925786b09e0716159a58529a01cdfdf9baf01
Peter Rigii <theonlyrigii@gmail.com>
2023-06-27 20:49:03 +0300

    2 square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,19 +7,25 @@ Define a class Square with size attribute
 
 class Square:
     """
-        Represent a square
-	    """
+    Represent a square
+    """
 
-	        def __init__(self, size=0):
-		        """
-			        Initialize a new square
+    def __init__(self, size=0):
+        """
+        Initialize a new square
 
-				        Args:
-					            size (int): The size of the new square
-						            """
-							            if (not isinstance(size, int)):
-								                raise TypeError("size must be an integer")
-										        elif (size < 0):
-											            raise ValueError("size must be >= 0")
-												            else:
-													                self.__size = size
+        Args:
+        size (int): The size of the new square
+        """
+
+        if (not isinstance(size, int)):
+
+            raise TypeError("size must be an integer")
+
+        elif (size < 0):
+
+            raise ValueError("size must be >= 0")
+
+        else:
+
+            self.__size = size
