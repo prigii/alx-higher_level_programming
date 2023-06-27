12a0f1d3e1ed17448a0c5ff4282380193fbe5f4e
Peter Rigii <theonlyrigii@gmail.com>
2023-06-28 01:15:20 +0300

    101 square

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -71,12 +71,12 @@ class Square:
 
             not all(isinstance(num, int) for num in value) or
 
-        not all(num >= 0 for num in value)):
+            not all(num >= 0 for num in value)):
 
             raise TypeError("position must be a tuple of
                             2 positive integers")
 
-        self.__position = value
+            self.__position = value
 
     def area(self):
 
