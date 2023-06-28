23bd124fe9ee2bef0e0d9cc9c47511dd4d98fe5b
Peter Rigii <theonlyrigii@gmail.com>
2023-06-28 16:21:24 +0300

    101 square

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -50,7 +50,7 @@ class Square:
 
             raise ValueError("size must be >= 0")
 
-            self.__size = value
+        self.__size = value
 
     @property
     def position(self):
@@ -67,16 +67,15 @@ class Square:
 
         if (not isinstance(value, tuple) or
 
-            len(value) != 2 or
+                len(value) != 2 or
 
-            not all(isinstance(num, int) for num in value) or
+                not all(isinstance(num, int) for num in value) or
 
-            not all(num >= 0 for num in value)):
+                not all(num >= 0 for num in value)):
 
-            raise TypeError("position must be a tuple of
-                            2 positive integers")
+            raise TypeError("position must be a tuple of 2 positive integers")
 
-            self.__position = value
+        self.__position = value
 
     def area(self):
 
