c9b7bde9f54eaab6d217f2830aefd4dcd817b163
Peter Rigii <theonlyrigii@gmail.com>
2023-07-10 16:38:33 +0300

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Defines teh function lookup
+"""
+
+
+def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object
+    """
+
+    return dir(obj)
