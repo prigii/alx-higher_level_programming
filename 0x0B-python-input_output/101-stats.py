b006c7be817a36070e58d580f4f40d9ee79c73e7
Peter Rigii <theonlyrigii@gmail.com>
2023-07-12 17:00:56 +0300

    correction

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -48,9 +48,9 @@ if __name__ == "__main__":
                 list_line = [x for x in line.split(" ") if x.strip()]
                 magic.add_status_code(list_line[-2])
                 magic.size += int(list_line[-1].strip("\n"))
-	    except:
-		pass
-	    nlines +=1
+            except:
+                pass
+            nlines += 1
     except KeyboardInterrupt:
         magic.print_info()
         raise
