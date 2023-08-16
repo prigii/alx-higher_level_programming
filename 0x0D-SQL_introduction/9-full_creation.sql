0e5dc005554cc130e18698d0b076d34000408699
Peter Rigii <theonlyrigii@gmail.com>
2023-08-16 11:08:05 +0300

    full creation

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,14 @@
+CREATE TABLE second_table IN DATABASE hbtn_0c_0 AS (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+----- insert four records ----
+INSERT INTO 'second_table' (id, name, score)
+VALUES (1, "John", 10);
+INSERT INTO 'second_table' (id, name, score)
+VALUES (2, "Alex", 3);
+INSERT INTO 'second_table' (id, name, score)
+VALUES (1, "Bob", 14);
+INSERT INTO 'second_table' (id, name, score)
+VALUES (1, "George", 8);
