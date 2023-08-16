9080cd859e3adeee30939d78d5ae3d453025bbd6
Peter Rigii <theonlyrigii@gmail.com>
2023-08-16 14:50:29 +0300

    updated

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,9 +1,5 @@
 -- convert table character to utf8
+USE hbtn_0c_0
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
--- update name field with new character set
-ALTER TABLE first_table
-CHANGE name name VARCHAR(256)
-CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
