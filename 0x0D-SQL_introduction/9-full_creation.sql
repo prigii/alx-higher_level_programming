26270ad61376701784b87d231ceec6832eb4d490
Peter Rigii <theonlyrigii@gmail.com>
2023-08-16 14:08:24 +0300

    updated

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- creates a table second_table in the database 
-CREATE TABLE second_table IN DATABASE hbtn_0c_0 AS (id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 -- insert four records 
 INSERT INTO 'second_table' (id, name, score)
 VALUES (1, "John", 10);
