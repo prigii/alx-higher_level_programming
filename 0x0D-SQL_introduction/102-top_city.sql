8022176b1063e10fe8162b69596a89484da390f4
Peter Rigii <theonlyrigii@gmail.com>
2023-08-16 15:57:41 +0300

    added database

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 -- Display top 3 cities with max temp between July and August
 SELECT city, MAX(value) AS max_temp
 FROM temperatures
-WHERE MONTH(date) IN (7, 8)
+WHERE MONTH IN (7, 8)
 GROUP BY city
 ORDER BY max_temp DESC
 LIMIT 3;
