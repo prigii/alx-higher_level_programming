bd74f199eed64331c15a26a1c1488e1c9c4df641
Peter Rigii <theonlyrigii@gmail.com>
2023-08-16 19:53:22 +0300

    privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- lists all privileges of the MySQL users user_0d_1 and user_0d_2
-CREATE USER 'user_0d_1'@'localhost';
-GRANT ALL PRIVILIGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILIGES ON *.* TO 'user_0d_1'@'localhost' 
+AND 'user_0d_2'@'localhost';
