df8e575bdf79c637036e333eabc7121fbfd233f1
Peter Rigii <theonlyrigii@gmail.com>
2023-08-16 16:06:35 +0300

    updated

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,5 +1,5 @@
 -- Display top 3 cities with max temp between July and August
-SELECT city, MAX(value) AS avg_temp
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures
 WHERE month = 7 or month = 8
 GROUP BY city
