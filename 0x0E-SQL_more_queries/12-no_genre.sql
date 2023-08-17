ccd839e463246025608c267fc5d5a3459cefb306
Peter Rigii <theonlyrigii@gmail.com>
2023-08-17 20:29:57 +0300

    no genre

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,5 @@
+-- lists all shows
+SELECT shows.title, show_gen.genre_id FROM tv_shows AS shows
+LEFT JOIN tv_show_genres AS show_gen ON shows.id = show_gen.show_id
+WHERE show_gen.genre_id IS NULL
+ORDER BY shows.title, show_gen.genre_id;
