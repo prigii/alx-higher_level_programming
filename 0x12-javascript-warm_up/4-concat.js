bf7a4482469db9e7da00c637e8c91e7c60467341
Peter Rigii <theonlyrigii@gmail.com>
2023-09-11 22:43:31 +0300

    create a sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 if (process.argv[2] === undefined) {
-    console.log("undefined is undefined");
+  console.log('undefined is undefined');
 } else if (process.argv[3] === undefined) {
-    console.log(process.argv[2] + " is undefined");
+  console.log(process.argv[2] + ' is undefined');
 } else {
-    console.log(process.argv[2] + " is " + process.argv[3]);
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
 }
-
