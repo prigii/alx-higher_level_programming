30d249c0fde0d16b616bb425621b39bd3bdfb001
Peter Rigii <theonlyrigii@gmail.com>
2023-09-11 23:09:53 +0300

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+function computeFactorial (n) {
+  // Base case: factorial of 0 is 1
+  if (isNaN(n)) {
+    return 1;
+  } else if (n <= 1) {
+    return 1;
+  } else {
+    // Recursive case: factorial of n is n times factorial of (n-1)
+    return n * computeFactorial(n - 1);
+  }
+}
+
+const arg = parseInt(process.argv[2]);
+
+console.log('Factorial of', arg, 'is', computeFactorial(arg));
