53bbd2e962bc2fcc59cd44d5162a351b30b7e9c2
Peter Rigii <theonlyrigii@gmail.com>
2023-09-11 23:06:23 +0300

    add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -8,7 +8,7 @@ const arg2 = parseInt(process.argv[3]);
 
 if (!isNaN(arg1) && !isNaN(arg2)) {
   const result = add(arg1, arg2);
-  console.log('Result:', result);
+  console.log(result);
 } else {
-  console.log('Invalid input. Please provide two integer arguments.');
+  console.log('NaN');
 }
