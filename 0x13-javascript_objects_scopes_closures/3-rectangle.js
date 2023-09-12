4470c5145036dbcac1b3f9ad973490aa848dc0c6
Peter Rigii <theonlyrigii@gmail.com>
2023-09-12 18:23:30 +0300

    rectangle #3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      this.width = 0;
+      this.height = 0;
+    }
+  }
+
+  print () {
+    if (this.width === 0 || this.height === 0) {
+      console.log('Empty Object');
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        let row = '';
+        for (let j = 0; j < this.width; j++) {
+          row += 'X';
+        }
+        console.log(row);
+      }
+    }
+  }
+}
