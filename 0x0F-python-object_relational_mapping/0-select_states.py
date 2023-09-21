a5209b3fbbd3c822a29db8104336aeb813953cf7
Peter Rigii <theonlyrigii@gmail.com>
2023-09-21 16:30:35 +0300

    corrected

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
         host="localhost", port=3306, user=argv[1],
         passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM 'states' ORDER BY 'id' ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
