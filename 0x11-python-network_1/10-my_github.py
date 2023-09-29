4aea1bfb0af696244a1196696c871146720e1802
Peter Rigii <theonlyrigii@gmail.com>
2023-09-29 17:03:47 +0300

    my github

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
+"""
+import requests
+import sys
+
+
+def get_github_id():
+    username = sys.argv[1]
+    password = sys.argv[2]
+    response = requests.get("https://api.github.com/user",
+                            auth=(username, password))
+    try:
+        json_response = response.json()
+        print(json_response["id"])
+    except ValueError:
+        print("Not a valid JSON")
+
+
+get_github_id()
