958f087315f5e4c5f7e150210d712c33c9da9ded
Peter Rigii <theonlyrigii@gmail.com>
2023-09-29 16:59:05 +0300

    search api

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import requests
+import sys
+
+
+def search_user():
+    letter = sys.argv[1] if len(sys.argv) > 1 else ""
+    response = requests.post("http://0.0.0.0:5000/search_user",
+                             data={"q": letter})
+    try:
+        json_response = response.json()
+        if json_response:
+            print(f"[{json_response['id']}] {json_response['name']}")
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
+
+
+search_user()
