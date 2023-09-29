b6884b005abf1bc2986842c207e90b3671f730ee
Peter Rigii <theonlyrigii@gmail.com>
2023-09-29 17:43:10 +0300

    post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,13 +3,15 @@
 POSTS an email using a given URL
 """
 import urllib.request
-import urllib.parse
 import sys
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    email = sys.argv[2]
-    data = urllib.parse.urlencode({"email": email}).encode()
-    with urllib.request.urlopen(url, data) as response:
-        body = response.read().decode("utf-8")
-        print(f"Your email is:\n\n{body}")
+url = sys.argv[1]
+email = sys.argv[2]
+
+data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+req = urllib.request.Request(url, data=data, method='POST')
+
+with urllib.request.urlopen(req) as response:
+    body = response.read().decode('utf-8')
+
+print(body)
