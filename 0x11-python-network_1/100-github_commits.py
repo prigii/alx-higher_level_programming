1a61e8339d016d910b76630ecc2eecc17b07b71b
Peter Rigii <theonlyrigii@gmail.com>
2023-09-29 17:20:36 +0300

    last 10 commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+uses the GitHub API to list the 10 most recent commits of
+the repository “rails” by the user “rails”
+"""
+import requests
+import sys
+
+
+def solve_challenge():
+    repository_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    response = requests.get(f"https://api.github.com/repos/\
+        {owner_name}/{repository_name}/commits")
+    try:
+        json_response = response.json()
+        for commit in json_response[:10]:
+            sha = commit["sha"]
+            author_name = commit["commit"]["author"]["name"]
+            print(f"{sha}: {author_name}")
+    except ValueError:
+        print("Not a valid JSON")
+
+
+solve_challenge()
