5f352c62d9fe2b0bdbeaf0cf22a31f2de047854a
Peter Rigii <theonlyrigii@gmail.com>
2023-09-29 16:21:13 +0300

    header send url

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -9,5 +9,4 @@ if __name__ == "__main__":
     url = sys.argv[1]
     with urllib.request.urlopen(url) as response:
         x_request_id = response.headers.get("X-Request-Id")
-        print(f"The value of the X-Request-Id variable in\
-            the response header is: {x_request_id}")
+        print(f"{x_request_id}")
