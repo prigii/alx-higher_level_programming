710c2df90af36744ebea98562710a13b35c65f76
Peter Rigii <theonlyrigii@gmail.com>
2023-09-30 12:01:26 +0300

    json api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,19 +7,19 @@ import requests
 import sys
 
 
-def search_user():
-    """ Function for searching a user """
-    letter = sys.argv[1] if len(sys.argv) > 1 else ""
-    response = requests.post("http://0.0.0.0:5000/search_user",
-                             data={"q": letter})
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        letter = sys.argv[1] 
+    else:
+        letter = ""
+    url = "http://0.0.0.0:5000/search_user"
+    data={"q": letter}
+    response = requests.post(url, data)
     try:
         json_response = response.json()
         if json_response:
             print(f"[{json_response['id']}] {json_response['name']}")
         else:
             print("No result")
-    except ValueError:
+    except Exception:
         print("Not a valid JSON")
-
-
-search_user()
