7bc0b5bc7c306f445a5bbc483dde956f0bc930d6
Peter Rigii <theonlyrigii@gmail.com>
2023-10-24 14:47:23 +0300

    edited

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,36 +2,17 @@
 // computes the number of tasks completed by user id
 
 const request = require('request');
-
-// Define the API URL
-const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
-
-// Send a GET request to the JSONPlaceholder API
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else if (response.statusCode !== 200) {
-    console.error(response.statusCode);
-  } else {
-    try {
-      const todos = JSON.parse(body);
-      const completedTasksByUser = {};
-
-      // Count the completed tasks for each user
-      todos.forEach((todo) => {
-        if (todo.completed) {
-          if (completedTasksByUser[todo.userId]) {
-            completedTasksByUser[todo.userId]++;
-          } else {
-            completedTasksByUser[todo.userId] = 1;
-          }
-        }
-      });
-
-      // Print the result in the specified format
-      console.log(completedTasksByUser);
-    } catch (parseError) {
-      console.error(parseError);
-    }
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    const completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
+      }
+    });
+    console.log(completed);
   }
 });
