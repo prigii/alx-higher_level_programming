d4c9cfc9223b90a0af77d553fd5f824844546124
Peter Rigii <theonlyrigii@gmail.com>
2023-10-25 10:37:51 +0300

    star wars season 3

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,40 @@
+#!/usr/bin/node
+// prints all characters of a Star Wars movie:
+const request = require('request');
+
+// Check if the movie ID is provided as a command-line argument
+if (process.argv.length < 3) {
+  console.log('Usage: node star-wars-characters.js <movie_id>');
+  process.exit(1);
+}
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+// Send a GET request to the Star Wars API
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else if (response.statusCode !== 200) {
+    console.error(response.statusCode);
+  } else {
+    try {
+      const movie = JSON.parse(body);
+
+        movie.characters.forEach((characterUrl, index) => {
+        request.get(characterUrl, (charError, charResponse, charBody) => {
+          if (charError) {
+            console.error(charError);
+          } else if (charResponse.statusCode !== 200) {
+            console.error(charResponse.statusCode);
+          } else {
+            const character = JSON.parse(charBody);
+            console.log(`${character.name}`);
+          }
+        });
+      });
+    } catch (parseError) {
+      console.error(parseError);
+    }
+  }
+});
