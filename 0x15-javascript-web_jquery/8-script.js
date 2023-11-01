260f47bbb77bb7f39d93244cbf50d2e3dbc09bcf
Peter Rigii <theonlyrigii@gmail.com>
2023-11-01 22:09:42 +0300

    debugged all taks

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,8 +1,8 @@
 $(function () {
   $.get('https://swapi.co/api/films/?format=json', function (data, textStatus) {
     if (textStatus === 'success') {
-      let films = data.results;
-      for (let i in films) {
+      const films = data.results;
+      for (const i in films) {
         $('#list_movies').append('<li>' + films[i].title + '</li>');
       }
     }
