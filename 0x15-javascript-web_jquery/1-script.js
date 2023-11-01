0e90da1e06ce5e864f573c3fbe08094da6dcaa19
Peter Rigii <theonlyrigii@gmail.com>
2023-11-01 16:38:45 +0300

    list of elements

--- 0x15-javascript-web_jquery/1-script.js
+++ 0x15-javascript-web_jquery/1-script.js
@@ -1,6 +1,6 @@
 (function() {
     // Use jQuery to select the <header> element by its tag name
-    var headerElement = $("header");
+    const headerElement = $("header");
 
     // Update the text color to red (#FF0000)
     headerElement.css("color", "#FF0000");
--- 0x15-javascript-web_jquery/README.md
+++ 0x15-javascript-web_jquery/README.md
@@ -2,3 +2,8 @@
 Tasks
 0. No JQuery
 1. With JQuery
+2. Click and turn red
+3. Add `.red` class
+4. Toggle classes
+5. List of elements
+6. Change the text
