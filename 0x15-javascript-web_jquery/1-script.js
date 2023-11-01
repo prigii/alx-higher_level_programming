2d41ab03c47abcc11069cccb924546fcfeb9184b
Peter Rigii <theonlyrigii@gmail.com>
2023-11-01 16:28:00 +0300

    task 1

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,7 @@
+(function() {
+    // Use jQuery to select the <header> element by its tag name
+    var headerElement = $("header");
+
+    // Update the text color to red (#FF0000)
+    headerElement.css("color", "#FF0000");
+});
--- 0x15-javascript-web_jquery/README.md
+++ 0x15-javascript-web_jquery/README.md
@@ -1,3 +1,4 @@
 0x15. JavaScript - Web jQuery
 Tasks
 0. No JQuery
+1. With JQuery
